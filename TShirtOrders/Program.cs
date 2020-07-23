a3ed2d64cda60ed8aea44132e70f5222abf46735
SERGIO MENDOZA GOMEZ <smendozagomez@cnm.edu>
2020-07-23 16:56:50 -0600

    Add project files.

---
+++ TShirtOrders/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Program.cs
+//Programmer: Rob Garner (rgarner7@cnm.edu)
+//Date: 10 Mar 2020
+//Purpose: Process a series of tshirt orders. 
+namespace TShirtOrders
+{
+    class Program
+    {
+        private const string COMPANYNAME = "Super Shirt Ordermatic 3000";
+        private const string COMPANYSLOGAN = "The best shirt ordering system in the multiverse!";
+
+        static void Main(string[] args)
+        {
+            Header();
+            char option;
+            List<TShirtOrder> orders = new List<TShirtOrder>();
+            do
+            {
+                DisplayShirtOrders(orders);
+                option = GetStringFromUser("(A)dd shirt (R)emove shirt (T)otal (E)xit: ").Trim().ToUpper()[0];
+                switch(option)
+                {
+                    case 'A':
+                        AddShirtOrder(orders);
+                        break;
+                    case 'R':
+                        RemoveShirtOrder(orders);
+                        break;
+                    case 'T':
+                        DisplayTotal(orders);
+                        break;
+
+                }                
+            } while (option!='E');
+            Console.WriteLine("Thank you for using "+ COMPANYNAME);
+        }
+        private static void DisplayTotal(List<TShirtOrder> orders)
+        {
+            decimal total = 0;
+
+            foreach (TShirtOrder order in orders)
+            {
+                total += order.Price;
+            }
+            Console.WriteLine("Total price of order is: " + total);
+        }
+
+        private static void RemoveShirtOrder(List<TShirtOrder> orders)
+        {
+            int index = GetIntFromUser("Enter index of shirt order to remove: ");
+            if (GetBoolFromUser("Are you sure you want to delete this order"))
+            {
+
+                //In Changed the Remove to ReoveAt to specify what index selection you want to remove.
+                orders.RemoveAt(index);
+            }
+        }
+        private static void AddShirtOrder(List<TShirtOrder> orders)
+        {
+            TShirtOrder order = new TShirtOrder();
+            order.FirstName = GetStringFromUser("Please enter your first name: ");
+            order.LastName = GetStringFromUser("Please enter your last name: ");
+            order.IsLocalPickup = GetBoolFromUser("Local pickup");
+            if (!order.IsLocalPickup)
+            {
+                order.Address = GetStringFromUser("Address: ");
+            }
+            order.OrderDate = DateTime.Now;
+            order.PrintAreaInSqIn = GetDoubleFromUser("Please enter are of your design in square inches: ");
+            order.NumColors = GetIntFromUser("Please enter number of colors: ");
+            order.NumShirts = GetIntFromUser("Please enter the number of shirts: ");
+            Console.WriteLine(order.ToString());
+            orders.Add(order);
+        }
+        private static void DisplayShirtOrders(List<TShirtOrder> orders)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Current shirts orders:");
+            if (orders.Count > 0)
+            {
+                //IN: Took the paraemthesis of so visual studio will not read iCount as a method
+                for (int i = 0; i < orders.Count; ++i)
+                {
+                    Console.WriteLine((i + 1) + ": " + orders[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No shirts in order.");
+            }
+            Console.WriteLine();
+        }
+        private static void Header()
+        {
+            Console.WriteLine("Welcome to "+ COMPANYNAME+"!");
+            Console.WriteLine(COMPANYSLOGAN);
+            Console.WriteLine();
+        }
+        private static bool GetBoolFromUser(string prompt)
+        {
+            Console.Write(prompt + " (y/n)? ");
+            return Console.ReadLine().ToLower()[0] == 'y';
+        }
+        private static int GetIntFromUser(string prompt)
+        {
+            Console.Write(prompt);
+            return int.Parse(Console.ReadLine());
+        }
+        private static double GetDoubleFromUser(string prompt)
+        {
+            Console.Write(prompt);
+            return int.Parse(Console.ReadLine());
+        }
+
+        private static string GetStringFromUser(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ TShirtOrders/TShirtOrder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+//TShirtOrder.cs
+//Programmer: Rob Garner (rgarner7@cnm.edu)
+//Date: 10 Mar 2020
+//Purpose: Model a TShirt order.
+namespace TShirtOrders
+{
+    /// <summary>
+    /// TShirtOrder
+    /// Provides a model of a shirt order.
+    /// </summary>
+    public class TShirtOrder
+    {                                                                                                                               //IN: printAreaSqIn had to inititalized like the rest of the parameters
+        public TShirtOrder(string firstName="",string lastName = "", string address = "", DateTime? orderDate=null,bool isLocalPickup=true, double printAreaInSqIn = 1, int numColors=1,int numShirts=1)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            OrderDate = orderDate;
+            IsLocalPickup = isLocalPickup;
+            this.printAreaInSqIn = printAreaInSqIn;
+            this.numColors = numColors;
+            this.numShirts = numShirts;
+            Calc();
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public bool IsLocalPickup { get; set; }
+        private double printAreaInSqIn;
+        public double PrintAreaInSqIn
+        {
+            get { return printAreaInSqIn; }
+            set { printAreaInSqIn = value; Calc(); }
+        }
+
+        private int numColors;
+        public int NumColors
+        {
+            //change property name to the field name
+            get { return numColors; }
+            set { numColors = value; Calc(); }
+        }
+
+        private int numShirts;
+        public int NumShirts
+        {
+            get { return numShirts; }
+            set { numShirts = value; Calc(); }
+        }
+        //IN change the accessor public by removing the private accessor
+        public decimal Price {  get; set; }
+        private void Calc()
+        {
+            //IN needed to convert numShirts to a decimal 
+            Price = Convert.ToDecimal(numShirts * (numColors * 2.25 + printAreaInSqIn * .05));
+        }
+        public override string ToString()
+        {
+            return FirstName + " "
+                + LastName + " "
+                + OrderDate.ToString()//Had to remove this from the parenthesis=> "MM/dd/yyyy HH:mm:ss")
+                +" Price: "+Price.ToString("c");
+        }
+    }
+}
